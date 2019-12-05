69fdc93a9b306983bb5d6982778e252ff77cd660
David Fernandez Chaves <43130031+David-Fernandez-Chaves@users.noreply.github.com>
2019-12-05 19:59:41 +0100

    Fix all

--- Assignment1.m
+++ Assignment1.m
@@ -1,61 +1,78 @@
-%David Fernández Chaves
+%David Fernández Chaves & Shafeef
 
-Alfa = 0.75:0.25:3;
+%General initialization
+Alpha = 0.75:0.25:3;
+Pls = zeros(1,size(Alpha,2));
+Qls = zeros(1,size(Alpha,2));
 
-Ndimensions=20;
-Iterations=100;
-P=round(Alfa*Ndimensions);
+Ndimensions=20;             %Number of dimensions
+Nmax=100;                   %Number max of iteration to learnd
+nD=50;                      %Number of experiment to get the mean
+P=round(Alpha*Ndimensions);  %Number of samples based on Alfa
+C=0.5;                      %Constant C>0
 
-figure
-hold ON
-
-%Apart E
-for ii = 1:size(P,2)
-    for k=1:nd
+%Each diferent Alfa
+for s_alfa = 1:size(Alpha,2)
+    
+    p = P(s_alfa);
+    
+    %Each diferent nD set of D
+    for s_nd=1:nD
         %Initialization
         Bias = 0;
-        C=0.5;
 
         Weights = [zeros(1,Ndimensions),Bias];
+        Input = [normrnd(0,1,Ndimensions,p);-ones(1,p)];
+        Output = (randi([0 1],p,1)*-2)+1;
 
-        Input = [normrnd(0,1,Ndimensions,P(ii));-ones(1,P(ii))];
-        Output = (randi([0 1],P(ii),1)*-2)+1;
-
-        %Loop learning proceed
-        for i = 1:Iterations
-            out = zeros(4,1);
+        %Loop learning process
+        for iteration = 1:Nmax
             found = 0;
 
             %Loop each samples
-            for j = 1:P(ii)
+            for step = 1:p
 
-                out(j) = Weights*Input(:,j)*Output(j);
+                out = Weights*Input(:,step)*Output(step);
 
-                if out(j) <= C
-                    Weights = Weights + (Input(:,j)'*Output(j))/Ndimensions;
+                if out <= C
+                    Weights = Weights + (Input(:,step)'*Output(step))/Ndimensions;
                 else
                     found = found +1;
                 end          
             end
 
-            if found == P
+            if found == p
+                Qls(s_alfa) = Qls(s_alfa) + 1;
                 break
             end
-        end
-
-        %Calculate Pls
-
-        for j=0:Ndimensions-1
-           Cpn =  factorial(P(ii)-1)/(factorial(j)*factorial(P(ii)-j-1));
-        end
-        Pls = 2*Cpn/pow(2,P(ii));
+        end     
     end
+    
+    Qls(s_alfa) = Qls(s_alfa)/nD;
+    
+    %Calculate Pls (Theorical)
+    Cpn=0;
+    if p<=Ndimensions
+        Pls(s_alfa) = 1;
+    else
+        for j=0:(Ndimensions-1)
+            Cpn = Cpn +  factorial(p-1)/(factorial(j)*factorial(p-1-j));
+        end
+        Pls(s_alfa) = 2^(1-p)*Cpn;
+    end    
+    
 end
 
+Pls
+Qls
+
+figure
+hold on
+title('Comparison between Pls and Qls')
+plot(Alpha,Pls)
+plot(Alpha,Qls)
+legend('Pls','Qls')
+xlabel('Alpha values') 
+ylabel('Probabilities') 
 
 
-
-
-solved
-Weights*Input
-Output
